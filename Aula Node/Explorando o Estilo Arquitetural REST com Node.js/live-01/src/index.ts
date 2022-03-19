15f321273045293de47f23e7bbec28c6442ac922
Carlos Roberto <carlos.ti1099@gmail.com>
2022-03-19 17:10:35 -0400

    Adicionando o typescript e incrementando a forma de desenvolver

--- Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-01/src/index.ts
+++ Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-01/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Executei!');
+
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'Sucesso Total'});
+});
+
+
+app.listen(3000, () => {
+    console.log('Aplicação executando a porta 3000!');
+});
