d8eaff0f95c19edd5a33653137d2bdccb3079a7f
Carlos Roberto <carlos.ti1099@gmail.com>
2022-03-20 15:50:34 -0400

    Criando as primeiras rotas

---
+++ Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-01/src/routes/users.route.ts
@@ -0,0 +1,16 @@
+import { Router, Response, Request, NextFunction } from "express";
+
+// get /users
+// get /users/:uuid
+// post /users
+// put /users/:uuid
+// delete /users/:uuid
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    const users = [{ userName: 'Carlos' }];
+    res.status(200).send({users});
+});
+
+export default usersRoute;
