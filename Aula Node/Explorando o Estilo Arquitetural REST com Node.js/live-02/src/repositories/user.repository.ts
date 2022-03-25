02526d841711f3d30a836d09bd6e7b1971e2af7c
carlosti1099 <carlos.ti1099@gmail.com>
2022-03-25 11:22:40 -0400

    Desenvolvendo o update e o delete de usuários

--- Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-02/src/repositories/user.repository.ts
+++ Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-02/src/repositories/user.repository.ts
@@ -45,6 +45,29 @@ class UserRepository {
         return newUser.uuid;
     }
 
+    async update(user: User): Promise<void> {
+        const script = `
+            UPDATE application_user 
+            set
+                username = $1,
+                password = crypt($2, 'my_salt')
+            WHERE uuid = $3
+        `;
+
+        const values = [user.username, user.password, user.uuid];
+        await db.query(script, values);
+    }
+
+    async remove(uuid: string): Promise<void> {
+        const script = `
+            DELETE
+            FROM application_user
+            WHERE uuid = $1
+        `;
+        const values = [uuid];
+        await db.query(script, values);
+    }
+
 }
 
 export default new UserRepository();
--- Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-02/src/routes/users.route.ts
+++ Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-02/src/routes/users.route.ts
@@ -29,16 +29,20 @@ usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction
     res.status(StatusCodes.CREATED).send(uuid);
 });
 
-usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.put('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     const modifiedUser = req.body;
 
     modifiedUser.uuid = uuid;
 
+    await userRepository.update(modifiedUser);
+
     res.status(StatusCodes.OK).send(modifiedUser);
 });
 
-usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.delete('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    await userRepository.remove(uuid);
     res.sendStatus(StatusCodes.OK);
 });
 
