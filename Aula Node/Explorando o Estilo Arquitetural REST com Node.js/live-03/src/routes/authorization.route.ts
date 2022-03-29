807c02e5e63269a91a2667dd5c4f30d22bae80f1
carlosti1099 <carlos.ti1099@gmail.com>
2022-03-29 17:45:22 -0400

    Instalação da biblioteca JWT para geração de tokens

--- Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-03/src/routes/authorization.route.ts
+++ Aula Node/Explorando o Estilo Arquitetural REST com Node.js/live-03/src/routes/authorization.route.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import ForbiddenError from "../models/error/forbidden.error.model";
 import userRepository from "../repositories/user.repository";
+import JWT from 'jsonwebtoken';
 
 const authorizationRoute = Router();
 
@@ -30,6 +31,14 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next: Next
 
         const user = await userRepository.findAllUsersnameAndPassword(username, password);
         console.log(user);
+
+        // "iss" o domínio da aplicação geradora do token
+        // "sub" É o assunto do token, mas é muito utilizado para guarda o ID do usuário
+        // "aud" Define quem pode usar o token
+        // "exp" Date para expiração do token
+        // "nbf" Define uma data para qual o token não pode ser aceito antes dela
+        // "iat" Data de criação do token
+        // "jti" o id do token
 
     } catch (error) {
        next(error);
